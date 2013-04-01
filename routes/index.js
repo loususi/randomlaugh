32a168a97940c4552dc2b43d3e15139807de8044
Rachael Stedman <rkstedman@gmail.com>
2013-03-31 20:48:11 -0400

    added it customized laugh

--- routes/index.js
+++ routes/index.js
@@ -44,30 +44,32 @@ exports.laugh = function(req, res){
   
   getRandomLaugh( function(laugh) {
     var phone_message = "Hello, " + from_name + " has sent you a laugh";
-    writeCallInstructions(phone_message, function(call_instructions_path) {
+    var laugh_path = global.domain[global.environment] + '/laughtracks/'+ laugh;
+    writeCallInstructions({phone_message: phone_message, laugh_path : laugh}, function(call_instructions_path) {
 
       var call_instructions_path = call_instructions_path != null ? call_instructions_path : global.domain[global.environment] + '/twilio/laugh.xml';
       console.log('Number to Call: ' + number_to_call);
       console.log('Laugh File: ' + laugh);
       console.log('Call Instructions Path: ' + call_instructions_path);
+      console.log("Laugh Path:" + laugh_path);
       console.log("From name:" + from_name);
 
-        // Place a phone call, and respond with TwiML instructions from the given URL
-        client.makeCall({
-            to: number_to_call, // Any number Twilio can call
-            from: global.twilio[global.environment].number, // A number you bought from Twilio and can use for outbound communication
-            url : call_instructions_path // A URL that produces an XML document (TwiML) which contains instructions for the call 
-        }, function(err, responseData) {
-            console.log('CALL HAS BEEN INITIATED');
-            res.send({status: "success"});
-        });
+        // // Place a phone call, and respond with TwiML instructions from the given URL
+        // client.makeCall({
+        //     to: number_to_call, // Any number Twilio can call
+        //     from: global.twilio[global.environment].number, // A number you bought from Twilio and can use for outbound communication
+        //     url : call_instructions_path // A URL that produces an XML document (TwiML) which contains instructions for the call 
+        // }, function(err, responseData) {
+        //     console.log('CALL HAS BEEN INITIATED');
+        //     res.send({status: "success"});
+        // });
     });
 
   });
 }
 
 function getRandomLaugh(callback) {
-  var path = currentDirectory() + 'laughtracks';
+  var path = currentDirectory() + 'public/laughtracks';
   fs.readdir(path, function(err, file_names) {
     //TODO handle err
     var laugh = file_names[Math.floor(Math.random()*file_names.length+1)];
@@ -75,14 +77,16 @@ function getRandomLaugh(callback) {
   });
 }
 
-function writeCallInstructions(phone_message, callback) {
-
-  var call_id = Math.floor(Math.random()*1000000000+1);
+function writeCallInstructions(params, callback) {
+  var phone_message = params.phone_message;
+  var laugh_path = params.laugh_path;
+  var call_id = (new Date()).getTime();
 
   //Build XML with package xmlbuilder
   var call_instructions_path = null;
   var xml = builder.create('Response')
     .ele('Say', {'voice': 'woman','language':'en'}, phone_message)
+    .ele('Audio', laugh_path)
     .end({ pretty: true});
 
   //Save XML to file\
